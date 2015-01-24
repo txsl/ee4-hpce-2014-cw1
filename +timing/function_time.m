68ca65c5799657b3d399d0d239b44894f7c0429a
Thomas <txl11@imperial.ac.uk>
2015-01-24 22:14:02 +0000

    Exercise 1 code complete. Need to produce graphs.

---
+++ +timing/function_time.m
@@ -0,0 +1,47 @@
+function [ t ] = function_time( f )
+% function_time Return the execution time for a single execution of f() in seconds
+% 
+%  f : A function with no inputs
+% 
+% The timing accuracy is 10% or better. So if the true time is
+% "t" and the measured time is "m", it should attempt to ensure
+% that abs((m-t)/t) < 0.1". This function supports
+% execution times of f from micro-seconds to minutes
+% efficiently.
+%
+% The user of this function takes on responsibility for
+% making sure that the machine is not loaded, and that
+% frequency scaling is turned off.
+%    
+% Examples:
+%  
+% > f=@()( sin(1) );  timing.function_time(f)
+%
+% > timing.function_time(@()( randn(1000)*randn(1000) ) )
+
+NUM_ITERS = 10;
+
+
+% run it once to ensure the JIT compiles it - including the timing
+% function!
+timing.simple_function_time(f());
+
+t1 = timing.simple_function_time(f());
+t2 = timing.simple_function_time(f());
+
+avg = mean([t1 t2]);
+
+if avg > 0.5
+    t = avg;
+elseif abs((t2-t1)/t1) >= 0.1
+    timings = zeros(NUM_ITERS, 1);
+    for i=1:NUM_ITERS
+        timings(i) = timing.simple_function_time(f());
+    end
+    t = mean(timings);
+else
+    t = avg;
+end
+
+end
+
---
+++ +timing/function_time_against_n.m
@@ -0,0 +1,47 @@
+function [ts,ns]=function_time_against_n(f, ns, maxTime)
+% function_time_against_n Measure the execution time of a function with varying inputs
+%
+%  f       : A function taking a single positive integer parameter n. Execution time
+%            is assumed to increase monotonically (approximately) with n.
+%  ns      : A vector of monotonically increasing positive integers.
+%  maxTime : Maximum amount of time the function should take (default == 3 seconds)
+%  
+%  This function calculates the changing execution time for a
+%  function of a single parameter, as that parameter takes on
+%  a vector of increasing values. The total time used to calculate
+%  the times should be (approximately) bounded by the maxTime
+%  parameter, so if maxTime is 60, then the user is willing to wait
+%  one minute for function_time_against_n to complete. The function
+%  should investigate as many data-points as possible - if time runs out, all
+%  remaining ts values should be NaN (Not a Number).
+%
+%  Usage:
+%
+%  > f=@(n)( randn(n).^2 );
+%  > timing.function_time_against_n(f, 1:20)
+%
+%  > [ts,ns]=timing.function_time_against_n(@(n)( randn(10)^n, 1:20) ); plot(ns,ts);
+%
+%  > [ts,ns]=timing.function_time_against_n(@(n)( inv(randn(n), 1:100), 10.0)  % Increase maxTime
+
+if nargin < 3
+    maxTime = 3;
+end
+
+num_iter = size(ns, 2);
+
+ts = nan(num_iter, 1);
+
+time_taken = 0;
+for i=1:num_iter
+    tic;
+    
+    g = @()(f(ns(i)));
+    ts(i) = timing.function_time(g);
+    
+    time_taken = time_taken + toc;
+    
+    if time_taken > maxTime
+        return
+    end
+end
