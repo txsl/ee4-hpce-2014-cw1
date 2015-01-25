d9a6d99ee0d59f7a451a1c1947fada1a05be3b0b
Thomas <txl11@imperial.ac.uk>
2015-01-25 12:15:29 +0000

    scharr_scalar (E2.1) now functioning as expected

--- +effects/scharr_scalar.m
+++ +effects/scharr_scalar.m
@@ -1,12 +1,19 @@
 function [ pixel ] = scharr_scalar( in )
-%SCHARR_SCALAR Summary of this function goes here
-%   Detailed explanation goes here
+% scharr_scalar Perform the Scharr kernel on a 3x3 matrix
+%   in - 3x3 matrix of neighbouring greyscale pixels
+%
+%   Returns: pixel, the difference between the inputs (as one value).
+%   Usefel for edge detection.
 
-G_x = 
-G_y = 
+G_x = ([ 3 10 3; 0 0 0; -3 -10 -3 ] .* in);
+G_y = ([ 3 0 -3; 10 0 -10; 3 0 -3 ] .* in);
+
+G_x = sum(G_x(:));
+G_y = sum(G_y(:));
 
 G = sqrt(G_x^2 + G_y^2);
 
 pixel = min(G/8, 1);
+
 end
 
