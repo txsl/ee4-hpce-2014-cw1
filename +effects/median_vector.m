2fe553987f3e6b8aa85ad9872e0c6933ea674e99
Thomas <txl11@imperial.ac.uk>
2015-01-27 22:01:47 +0000

    median_vector now working!!

--- +effects/median_vector.m
+++ +effects/median_vector.m
@@ -1,7 +1,23 @@
 function [ pixel_row ] = median_vector( in )
-%MEDIAN_VECTOR Summary of this function goes here
-%   Detailed explanation goes here
-med_line = median(in)
-pause
+% median filter Calculate median pixel of input matrix
+%   
+%   in - Square input vec of odd height (ie 1, 3, 5, etc high)
+%
+%   Returns pixel: the vector of median pixels
+
+w_out = size(in, 1);
+length = size(in, 2);
+
+border = w_out/2 - 0.5;
+
+output_len = length-2*border;
+
+index_trix = transpose(1:9);
+index_trix = repmat(index_trix, [1, output_len]);
+
+index_trix = index_trix + 3*repmat(0:output_len-1, [9, 1]);
+
+pixel_row = median(in(index_trix));
+
 end
 
